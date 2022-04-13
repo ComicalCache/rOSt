1cf61d4f71906b562a1d1823d7fa6937c5e40fac
0xffset <rene.gottschalk02@gmail.com>
2022-04-13 21:44:37 +0200

    documented interrupts

--- src/interrupts/gtd.rs
+++ src/interrupts/gtd.rs
@@ -1,19 +1,25 @@
-use x86_64::VirtAddr;
-use x86_64::registers::segmentation::SegmentSelector;
-use x86_64::structures::gdt::{GlobalDescriptorTable, Descriptor};
-use x86_64::structures::tss::TaskStateSegment;
 use lazy_static::lazy_static;
+use x86_64::registers::segmentation::SegmentSelector;
+use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable};
+use x86_64::structures::tss::TaskStateSegment;
+use x86_64::VirtAddr;
 
+/// the interrupt stack table index of the stack used for double faults
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 
 lazy_static! {
+    /// The TSS of the OS.
     static ref TSS: TaskStateSegment = {
         let mut tss = TaskStateSegment::new();
+
+        // set the interrupt stack table to the appropriate address
         tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
             const STACK_SIZE: usize = 4096 * 5;
             static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
             let stack_start = VirtAddr::from_ptr(unsafe { &STACK });
+
+            // returns the highest address of the stack because the stack grows downwards
             let stack_end = stack_start + STACK_SIZE;
             stack_end
         };
@@ -22,11 +28,19 @@ lazy_static! {
 }
 
 lazy_static! {
+    /// The GDT used by the OS.
     static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
+
         let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
         let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
-        (gdt, Selectors { code_selector, tss_selector })
+        (
+            gdt,
+            Selectors {
+                code_selector,
+                tss_selector,
+            },
+        )
     };
 }
 
@@ -36,9 +50,10 @@ struct Selectors {
 }
 
 pub fn init_gdt() {
+    use x86_64::instructions::segmentation::{Segment, CS};
     use x86_64::instructions::tables::load_tss;
-    use x86_64::instructions::segmentation::{CS, Segment};
-    
+
+    // sets our TSS as the active task state segment
     GDT.0.load();
     unsafe {
         CS::set_reg(GDT.1.code_selector);
--- src/interrupts/register.rs
+++ src/interrupts/register.rs
@@ -4,12 +4,17 @@ use x86_64::structures::idt::InterruptDescriptorTable;
 use crate::interrupts::handlers::{breakpoint_handler, double_fault_handler};
 
 lazy_static! {
+    /// The IDT used by the OS.
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
+
+        // sets all the interrupt handlers for the appropriate interrupts
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+
         unsafe {
             idt.double_fault
                 .set_handler_fn(double_fault_handler)
+                // changes stack for double fault to avoid triple faults
                 .set_stack_index(crate::interrupts::gtd::DOUBLE_FAULT_IST_INDEX);
         }
         idt
