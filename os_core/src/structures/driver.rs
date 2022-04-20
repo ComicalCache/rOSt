de0ff0d10255fa32fa35aa989c2b5161ead81b50
michalusio <micha_i@interia.pl>
2022-04-20 23:33:55 +0200

    Added a comment about extern drivers

--- os_core/src/structures/driver.rs
+++ os_core/src/structures/driver.rs
@@ -1,5 +1,6 @@
 use super::kernel_information::KernelInformation;
 
+/// extern C for future compatibility with loading drivers from the file system
 pub type Registrator = extern "C" fn(KernelInformation) -> Driver;
 
 #[derive(Clone, Copy)]
