0c986bf78828f0f99e66596f1b95295b0ea5a738
michalusio <micha_i@interia.pl>
2022-04-28 20:25:35 +0200

    Enabled workspace dependencies Made the toolchain channel more precise so the rust-analyzer works faster Changed the master build so it builds in release mode Added an intermediate PixelBuffer for VGA to make pixel drawing faster Changed the sub-packages naming scheme to lib-like Renamed lib to rost-lib

---
+++ drivers/vga/src/pixel_buffer.rs
@@ -0,0 +1,61 @@
+use kernel::structures::kernel_information::PixelFormat;
+use utils::div_255_fast;
+
+use crate::vga_color::VGAColor;
+
+pub trait PixelBuffer: Send {
+    fn put_pixel(&mut self, index: usize, color: VGAColor<u8>);
+}
+
+pub(crate) struct BasePixelBuffer<const P: PixelFormat> {
+    pub frame_pointer: &'static mut [u8],
+    pub bytes_per_pixel_shift: u8,
+}
+
+impl PixelBuffer for BasePixelBuffer<{ PixelFormat::RGB }> {
+    #[inline(always)]
+    fn put_pixel(&mut self, index: usize, color: VGAColor<u8>) {
+        let index = index << self.bytes_per_pixel_shift;
+        let frame_color = VGAColor {
+            red: self.frame_pointer[index + 0],
+            green: self.frame_pointer[index + 1],
+            blue: self.frame_pointer[index + 2],
+            alpha: self.frame_pointer[index + 3],
+        };
+        let result_color = VGAColor::interpolate(frame_color, color, color.alpha);
+        self.frame_pointer[index + 0] = result_color.red;
+        self.frame_pointer[index + 1] = result_color.green;
+        self.frame_pointer[index + 2] = result_color.blue;
+        self.frame_pointer[index + 3] = result_color.alpha;
+    }
+}
+
+impl PixelBuffer for BasePixelBuffer<{ PixelFormat::BGR }> {
+    #[inline(always)]
+    fn put_pixel(&mut self, index: usize, color: VGAColor<u8>) {
+        let index = index << self.bytes_per_pixel_shift;
+        let frame_color = VGAColor {
+            red: self.frame_pointer[index + 2],
+            green: self.frame_pointer[index + 1],
+            blue: self.frame_pointer[index + 0],
+            alpha: self.frame_pointer[index + 3],
+        };
+        let result_color = VGAColor::interpolate(frame_color, color, color.alpha);
+        self.frame_pointer[index + 2] = result_color.red;
+        self.frame_pointer[index + 1] = result_color.green;
+        self.frame_pointer[index + 0] = result_color.blue;
+        self.frame_pointer[index + 3] = result_color.alpha;
+    }
+}
+
+impl PixelBuffer for BasePixelBuffer<{ PixelFormat::U8 }> {
+    #[inline(always)]
+    fn put_pixel(&mut self, index: usize, color: VGAColor<u8>) {
+        let index = index << self.bytes_per_pixel_shift;
+        let gray = self.frame_pointer[index] as u16;
+        let color_gray = color.to_grayscale() as u16;
+        let alpha = color.alpha as u16;
+        let alpha1 = 255 - alpha;
+        self.frame_pointer[index] = div_255_fast(gray * alpha1 + color_gray * alpha);
+    }
+}
